4aebd5001b736629ee297b189b8be4bdcebf4ce8
Sascha <butschas@gmail.com>
2026-07-20 16:15:11 +0300

    Update Latvijas Labumi featured image

---
+++ supabase/migrations/20260720163000_update_latvijas_labumi_featured_image.sql
@@ -0,0 +1,7 @@
+update public.partners
+set portrait_url = '/partners/latvijas-labumu-tirgus-mals/images/056360081805de.jpg'
+where slug = 'latvijas-labumu-tirgus-mals';
+
+update public.artisans
+set portrait_url = '/partners/latvijas-labumu-tirgus-mals/images/056360081805de.jpg'
+where slug = 'latvijas-labumu-tirgus-mals';
